9f46dceb40a987a4c8d86b4e94b294221b1cf018
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-17 01:11:27 +0000

    Adding tarfile member sanitization to extractall()

--- omeganlp/core/io.py
+++ omeganlp/core/io.py
@@ -83,7 +83,26 @@ def tgz(source_dir: str, output_path: Optional[str] = None) -> str:
 
 def untgz(source_path: str, target_dir: str = ".") -> None:
     with tarfile.open(source_path, "r") as tar:
-        tar.extractall(path=target_dir)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, path=target_dir)
 
 
 def is_empty(path: str) -> Optional[bool]:
